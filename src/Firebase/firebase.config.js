742f952d711c4ec6bdfee6f4b4e5b16939241779
DEWAN <dewanmahinchy@gmail.com>
2021-10-19 15:28:00 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdl46Co2JDjF9-fW6tDwArb20_NniwIe4",
+    authDomain: "medicality-eafba.firebaseapp.com",
+    projectId: "medicality-eafba",
+    storageBucket: "medicality-eafba.appspot.com",
+    messagingSenderId: "165117821879",
+    appId: "1:165117821879:web:7389eb2297a55e1ea21e32"
+};
+
+export default firebaseConfig;
