a1b82ccb101fd5ee5662a262f31db42bfa7a3f64
DEWAN <dewanmahinchy@gmail.com>
2021-10-20 23:58:58 +0600

    done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-            <h1>404 error</h1>
+            <h1 className="error">404 error</h1>
+            <Link to='/home' className="auth-button text-center">Go to home</Link>
         </div>
     );
 };
